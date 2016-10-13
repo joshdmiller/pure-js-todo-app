01229f71b2bbe8537abc493c99e1f2352aa4a45d
Josh David Miller <josh@joshdmiller.com>
2016-10-13 14:27:45 -0700

    chore: add webpack-dev-server with HMR

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+
+module.exports = {
+  devtool: 'inline-source-map',
+
+  entry: './src',
+
+  output: {
+    path: path.join( __dirname, 'dist' ),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        include: path.join( __dirname, 'src' ),
+      },
+    ],
+  }
+};
+
