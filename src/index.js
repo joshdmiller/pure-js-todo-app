32e019ea43d605d27d89d0e9b4d10e65396ee3ef
Josh David Miller <josh@joshdmiller.com>
2016-12-16 17:00:08 -0800

    feat: add webpack dev server
    Add a configuration for webpack and its dev server to serve up the app
    on port 8080.
    
    Add an index.html file for the server to serve up, which will host our
    app.
    
    Fix a bug in `src/index.js`: named import to default import.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { TodoApp } from './TodoApp';
+import TodoApp from './TodoApp';
 
 const app = TodoApp();
+
+/**
+ * expose the app on the `window` object so we can play with it in the console.
+ */
+window.app = app;
 
