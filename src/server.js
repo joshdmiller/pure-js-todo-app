671b23832ce5eac170171cdaa14534db14b16fdd
Josh David Miller <josh@joshdmiller.com>
2017-01-05 16:16:06 -0800

    feat: create REST API for todos

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import db from './db';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+app.use( cors() );
+app.use( bodyParser() );
+
+app.get( '/', ( req, res ) => {
+  res.send( 'hello' );
+});
+
+app.get( '/todos', ( req, res ) => {
+  db.getTodos().then( todos => res.send( todos ) );
+});
+
+app.post( '/todos', ( req, res ) => {
+  const title = req.body.title;
+
+  db.createTodo( title ).then( todo => res.send( todo ) );
+});
+
+app.delete( '/todos/:id', ( req, res ) => {
+  db.rmTodo( req.params.id ).then( () => res.send({ status: 200, message: 'success' }) );
+});
+
+app.put( '/todos/:id', ( req, res ) => {
+  db.changeTodo( req.params.id, req.body ).then( todo => res.send( todo ) );
+});
+
+app.listen( 3000, function () {
+  console.log( 'Server is running!' );
+});
+
